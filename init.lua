605e6f7269fa99bd99180475137116ace56f3f88
İlyas Akın <ashnwor@gmail.com>
2023-10-28 02:23:43 +0300

    Add more filetypes to conform

--- init.lua
+++ init.lua
@@ -563,6 +563,9 @@ require('conform').setup {
   formatters_by_ft = {
     lua = { 'stylua' },
     javascript = { { 'prettierd', 'prettier' } },
+    typescript = { { 'prettierd', 'prettier' } },
+    javascriptreact = { { 'prettierd', 'prettier' } },
+    typescriptreact = { { 'prettierd', 'prettier' } },
   },
 }
 
