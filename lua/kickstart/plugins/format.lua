e34943d2315b051ed213ae0c701fbc861e8336da
İlyas Akın <ashnwor@gmail.com>
2023-10-28 03:15:24 +0300

    Move conform to format.lua plugin file

---
+++ lua/kickstart/plugins/format.lua
@@ -0,0 +1,27 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      javascript = { { 'prettierd', 'prettier' } },
+      typescript = { { 'prettierd', 'prettier' } },
+      javascriptreact = { { 'prettierd', 'prettier' } },
+      typescriptreact = { { 'prettierd', 'prettier' } },
+    },
+  },
+  config = function()
+    require('conform').setup {}
+
+    vim.api.nvim_create_user_command('Format', function(args)
+      local range = nil
+      if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ['end'] = { args.line2, end_line:len() },
+        }
+      end
+      require('conform').format { async = true, lsp_fallback = true, range = range }
+    end, { range = true, desc = 'Format current buffer with LSP' })
+  end,
+}
