c91cea8b859c838cf517af76c38008acb4837e59
İlyas Akın <ashnwor@gmail.com>
2024-02-10 01:54:36 +0300

    Disable vimscript plugins, replace indent detection plugin with lua counterpart

--- init.lua
+++ init.lua
@@ -51,6 +51,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure plugins ]]
@@ -63,11 +64,17 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  -- 'tpope/vim-fugitive',
+  -- 'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+  -- 'tpope/vim-sleuth',
+  {
+    'Darazaki/indent-o-matic',
+    config = function()
+      require('indent-o-matic').setup {}
+    end,
+  },
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
