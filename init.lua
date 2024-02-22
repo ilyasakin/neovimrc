ec6b3179b8de8feb337d5c3c1960880b62283619
İlyas Akın <ilyas.akin@kuika.com>
2024-02-22 22:08:56 +0300

    Remove unused plugin

--- init.lua
+++ init.lua
@@ -298,20 +298,6 @@ require('lazy').setup({
   },
 
   {
-    "SmiteshP/nvim-navbuddy",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "SmiteshP/nvim-navic",
-      "MunifTanjim/nui.nvim",
-      "numToStr/Comment.nvim",        -- Optional
-      "nvim-telescope/telescope.nvim" -- Optional
-    },
-    config = function()
-      require('nvim-navbuddy').setup()
-    end
-  },
-
-  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
