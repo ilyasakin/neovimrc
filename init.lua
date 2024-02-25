13a8d0a1ce8773d3cc0449c72ef327441d7d37e3
İlyas Akın <ashnwor@gmail.com>
2024-02-25 15:59:32 +0300

    Add lspkind

--- init.lua
+++ init.lua
@@ -115,6 +115,7 @@ require('lazy').setup({
         end)(),
       },
       'saadparwaiz1/cmp_luasnip',
+      'onsails/lspkind.nvim',
 
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
@@ -338,12 +339,6 @@ require('lazy').setup({
     'f-person/git-blame.nvim',
     event = 'VeryLazy',
     opts = {} -- this is equalent to setup({}) function
-  },
-  {
-    'sindrets/diffview.nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
   },
 
   require '0x000000.plugins.format',
@@ -798,7 +793,25 @@ local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 --
+local lspkind = require 'lspkind'
 cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      -- can also be a function to dynamically calculate max width such as
+      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        return vim_item
+      end,
+      symbol_map = { Copilot = "" }
+    })
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
