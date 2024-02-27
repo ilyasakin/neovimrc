37c6b6472dbf018931ad0642624c144fb9476a9f
İlyas Akın <ilyas.akin@kuika.com>
2024-02-27 20:51:54 +0300

    Move gitgutter into plugin file

--- init.lua
+++ init.lua
@@ -164,42 +164,7 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-  {
-    'airblade/vim-gitgutter',
-    event = 'BufRead',
-    keys = {
-      {
-        '<leader>hp',
-        ':GitGutterPreviewHunk<CR>',
-        mode = 'n',
-        desc = '[P]review git hunk',
-      },
-      {
-        '<leader>hs',
-        ':GitGutterStageHunk<CR>',
-        mode = 'n',
-        desc = '[S]tage git hunk',
-      },
-      {
-        '<leader>hu',
-        ':GitGutterUndoHunk<CR>',
-        mode = 'n',
-        desc = '[U]ndo git hunk',
-      },
-      {
-        ']c',
-        ':GitGutterNextHunk<CR>',
-        mode = 'n',
-        desc = 'Jump to [N]ext git hunk',
-      },
-      {
-        '[c',
-        ':GitGutterPrevHunk<CR>',
-        mode = 'n',
-        desc = 'Jump to [P]revious git hunk',
-      },
-    },
-  },
+  require '0x000000.plugins.gitgutter',
 
   {
     -- Theme inspired by Atom
