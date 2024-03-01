6c78d015b80b4dd2a96fc2ead7a11cff7a06e6ed
İlyas Akın <ashnwor@gmail.com>
2024-03-02 01:50:32 +0300

    Add screen centering remaps

--- lua/0x000000/remap.lua
+++ lua/0x000000/remap.lua
@@ -4,3 +4,9 @@ vim.keymap.set("v", "p", [["_dP]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
