c517ce2e5283fec10e3a0691dfd55155162b5017
İlyas Akın <ilyas.akin@kuika.com>
2024-03-12 17:31:10 +0300

    Add neodim

--- init.lua
+++ init.lua
@@ -317,6 +317,31 @@ require('lazy').setup({
     },
     opts = {},
   },
+
+  {
+    "zbirenbaum/neodim",
+    event = "LspAttach",
+    config = function()
+      require("neodim").setup({
+        refresh_delay = 75,
+        alpha = 0.75,
+        blend_color = "#000000",
+        hide = {
+          underline = true,
+          virtual_text = true,
+          signs = true,
+        },
+        regex = {
+          "[uU]nused",
+          "[nN]ever [rR]ead",
+          "[nN]ot [rR]ead",
+        },
+        priority = 128,
+        disable = {},
+      })
+    end
+  },
+
   require '0x000000.plugins.format',
   require '0x000000.plugins.copilot',
   require '0x000000.plugins.copilot-cmp',
