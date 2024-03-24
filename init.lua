721a4827e7474338b705e53edcd5bfa0bfea0555
İlyas Akın <ashnwor@gmail.com>
2024-03-24 14:42:16 +0300

    Add alias for which key category

--- init.lua
+++ init.lua
@@ -677,6 +677,7 @@ require('which-key').register {
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>q'] = { name = '[Q]uick', _ = 'which_key_ignore' }
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
