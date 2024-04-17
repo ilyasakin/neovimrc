e5343ac0e1ff75f5cafa16192c8ec3550329cd07
İlyas Akın <ilyas.akin@kuika.com>
2024-04-17 17:28:54 +0300

    Switch to copilot.vim

--- init.lua
+++ init.lua
@@ -806,6 +806,7 @@ mason_lspconfig.setup_handlers {
 }
 
 require("roslyn").setup({
+  roslyn_version = "4.9.2",
   on_attach = on_attach,
   capabilities = capabilities
 });
@@ -865,20 +866,6 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
   },
   sources = {
     { name = 'copilot' },
--- lua/0x000000/plugins/copilot-cmp.lua
+++ lua/0x000000/plugins/copilot-cmp.lua
@@ -1,5 +1,6 @@
 return {
   "zbirenbaum/copilot-cmp",
+  enabled = false,
   config = function()
     vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
     require("copilot_cmp").setup()
