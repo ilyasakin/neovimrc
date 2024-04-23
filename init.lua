fcd5a382ec49dbbc3fdd695b7314bfb8f150a9b5
İlyas Akın <ilyas.akin@kuika.com>
2024-04-23 05:34:14 +0300

    Re-enable gitgutter

--- init.lua
+++ init.lua
@@ -396,39 +396,6 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
-
--- Next error, previous error
-local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity })
-  end
-end
-
-vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-vim.keymap.set("n", "]h", diagnostic_goto(true, "HINT"), { desc = "Next Hint" });
-vim.keymap.set("n", "[h", diagnostic_goto(false, "HINT"), { desc = "Prev Hint" });
-
-vim.keymap.set('n', '<leader>f', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Open [E]xplorer' })
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
--- lua/0x000000/keymap.lua
+++ lua/0x000000/keymap.lua
@@ -1,39 +1,38 @@
--- keys = {
---   {
---     '<leader>gs',
---     ':G status<CR>',
---     mode = 'n',
---     desc = 'Git [S]tatus',
---   },
---   {
---     '<leader>gc',
---     ':G commit<CR>',
---     mode = 'n',
---     desc = 'Git [C]ommit',
---   },
---   {
---     '<leader>gp',
---     ':G push<CR>',
---     mode = 'n',
---     desc = 'Git [P]ush',
---   },
---   {
---     '<leader>gl',
---     ':G log<CR>',
---     mode = 'n',
---     desc = 'Git [L]og',
---   },
---   {
---     '<leader>gd',
---     ':Gvdiffsplit<CR>',
---     mode = 'n',
---     desc = 'Git [D]iff',
---   },
--- }
---
-
 vim.api.nvim_set_keymap('n', '<leader>gs', ':G status<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gc', ':G commit<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gp', ':G push<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gl', ':G log<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gd', ':Gvdiffsplit<CR>', { noremap = true, silent = true })
+
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+
+-- Next error, previous error
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+
+vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+vim.keymap.set("n", "]h", diagnostic_goto(true, "HINT"), { desc = "Next Hint" });
+vim.keymap.set("n", "[h", diagnostic_goto(false, "HINT"), { desc = "Prev Hint" });
+
+vim.keymap.set('n', '<leader>f', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Open [E]xplorer' })
