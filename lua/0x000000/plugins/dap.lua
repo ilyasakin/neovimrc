2877376206cf1432afb1faac908485fb65011b1e
İlyas Akın <ilyas.akin@kuika.com>
2024-04-23 06:43:04 +0300

    Merge dapui and dap

--- lua/0x000000/plugins/dap.lua
+++ lua/0x000000/plugins/dap.lua
@@ -1,73 +1,110 @@
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError" })
-vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DiagnosticInfo" })
-vim.fn.sign_define("DapStopped", { text = "", texthl = "Constant", linehl = "debugPC" })
-vim.fn.sign_define("DapBreakpointRejected", { text = "" })
-
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "jay-babu/mason-nvim-dap.nvim",
-    "LiadOz/nvim-dap-repl-highlights",
-    "theHamsta/nvim-dap-virtual-text",
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "jay-babu/mason-nvim-dap.nvim",
+      "LiadOz/nvim-dap-repl-highlights",
+      "theHamsta/nvim-dap-virtual-text",
+      "rcarriga/nvim-dap-ui",
+    },
+    keys = {
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Debug: Continue",
+      },
+      {
+        "<leader>dn",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Debug: Step over",
+      },
+      {
+        "<leader>di",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Debug: Step into",
+      },
+      {
+        "<leader>do",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Debug: Step out",
+      },
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Debug: Toggle breakpoint",
+      },
+      {
+        "<leader>dp",
+        function()
+          require("dap").set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
+        end,
+        desc = "Set log point",
+      },
+      {
+        "<leader>dr",
+        function()
+          require("dap").repl.toggle()
+        end,
+        desc = "Toggle REPL",
+      },
+      {
+        "<leader>dz",
+        function()
+          require 'dap'.set_exception_breakpoints()
+        end
+        ,
+        desc = "Set exception breakpoints"
+      }
+    },
+  },
+  {
     "rcarriga/nvim-dap-ui",
-  },
-  keys = {
-    {
-      "<leader>dc",
-      function()
-        require("dap").continue()
-      end,
-      desc = "Debug: Continue",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
     },
-    {
-      "<leader>dn",
-      function()
-        require("dap").step_over()
-      end,
-      desc = "Debug: Step over",
+
+    opts = {},
+    keys = {
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle { layout = 1 }
+        end,
+        desc = "Toggle UI sidebar",
+      },
+      {
+        "<leader>dU",
+        function()
+          require("dapui").toggle {}
+        end,
+        desc = "Toggle UI",
+      },
+      {
+        "<leader>dt",
+        function()
+          require("dapui").toggle { layout = 2 }
+        end,
+        desc = "Toggle console",
+      },
+      {
+        "<M-k>",
+        function()
+          require("dapui").eval()
+        end,
+        desc = "Evaluate expression",
+        mode = { "n", "v" },
+      },
     },
-    {
-      "<leader>di",
-      function()
-        require("dap").step_into()
-      end,
-      desc = "Debug: Step into",
-    },
-    {
-      "<leader>do",
-      function()
-        require("dap").step_out()
-      end,
-      desc = "Debug: Step out",
-    },
-    {
-      "<leader>db",
-      function()
-        require("dap").toggle_breakpoint()
-      end,
-      desc = "Debug: Toggle breakpoint",
-    },
-    {
-      "<leader>dp",
-      function()
-        require("dap").set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
-      end,
-      desc = "Set log point",
-    },
-    {
-      "<leader>dr",
-      function()
-        require("dap").repl.toggle()
-      end,
-      desc = "Toggle REPL",
-    },
-    {
-      "<leader>dz",
-      function()
-        require 'dap'.set_exception_breakpoints()
-      end
-      ,
-      desc = "Set exception breakpoints"
-    }
-  },
+  }
 }
--- lua/0x000000/signs.lua
+++ lua/0x000000/signs.lua
@@ -2,3 +2,9 @@ vim.fn.sign_define('DiagnosticSignError', { text = '!', texthl = 'DiagnosticSign
 vim.fn.sign_define('DiagnosticSignWarn', { text = '!', texthl = 'DiagnosticSignWarn' })
 vim.fn.sign_define('DiagnosticSignInfo', { text = 'i', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define('DiagnosticSignHint', { text = '?', texthl = 'DiagnosticSignHint' })
+
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError" })
+vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DiagnosticInfo" })
+vim.fn.sign_define("DapStopped", { text = "", texthl = "Constant", linehl = "debugPC" })
+vim.fn.sign_define("DapBreakpointRejected", { text = "" })
+
