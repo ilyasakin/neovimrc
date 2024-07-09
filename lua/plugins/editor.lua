c6862b8c4f305cdca44279a4be2ea099f697ed93
İlyas Akın <ilyas.akin@kuika.com>
2024-07-09 19:12:54 +0300

    Switch to roslyn.nvim again

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -31,4 +31,10 @@ return {
     event = 'VeryLazy',
     opts = {},
   },
+
+  {
+    "benlubas/molten-nvim",
+        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+  }
 }
