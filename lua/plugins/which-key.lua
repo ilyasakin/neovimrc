f305cac490d3ae84cd4f5fcd4f314b6478044168
İlyas Akın <ilyas.akin@kuika.com>
2024-08-25 23:56:33 +0300

    Fix which-key delay

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,17 +1,18 @@
 return {
   'folke/which-key.nvim',
-  opts = {},
   config = function()
-    require('which-key').add {
-      {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      },
-    }
+    local wk = require('which-key')
+    wk.setup({});
+    wk.add({
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>g', group = '[G]it' },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      { '<leader>q', group = '[Q]uick' },
+    });
   end,
 }
