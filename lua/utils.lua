929d776a8c450adbebf2ad1d8295c37c19e300f5
İlyas Akın <ilyas.akin@kuika.com>
2024-08-26 03:20:15 +0300

    Fix which key for lsp

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,11 +1,14 @@
 local M = {}
+local wk = require('which-key')
 
 M.lsp_nmap = function(keys, func, desc)
   if desc then
     desc = 'LSP: ' .. desc
   end
 
-  vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  wk.add({
+    { keys, func, desc = desc, mode = 'n' }
+  })
 end
 
 return M
