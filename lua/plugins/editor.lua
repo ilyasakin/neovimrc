9e41b6161254cd90b83b159e9f5f27fa7318a623
İlyas Akın <ashnwor@gmail.com>
2024-09-08 21:20:27 +0300

    Remove molten nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -32,10 +32,5 @@ return {
     opts = {},
   },
 
-  {
-    'benlubas/molten-nvim',
-    version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
-    build = ':UpdateRemotePlugins',
-  },
   { 'danilamihailov/beacon.nvim' },
 }
