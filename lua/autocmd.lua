a19f45210b35bd93c66d94697aed7eb2e7e4a8df
İlyas Akın <ashnwor@gmail.com>
2024-09-08 21:19:56 +0300

    Add diagnostics refresh autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -16,3 +16,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
+  pattern = '*',
+  callback = function()
+    local clients = vim.lsp.get_clients { name = 'roslyn' }
+    if not clients or #clients == 0 then
+      return
+    end
+
+    local buffers = vim.lsp.get_buffers_by_client_id(clients[1].id)
+    for _, buf in ipairs(buffers) do
+      vim.lsp.util._refresh('textDocument/diagnostic', { bufnr = buf })
+    end
+  end,
+})
