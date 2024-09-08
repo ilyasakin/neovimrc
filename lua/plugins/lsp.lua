28288fdbb3995449c6c475717e95ac07b2e8ff21
İlyas Akın <ashnwor@gmail.com>
2024-09-08 21:26:50 +0300

    Fix full semantic tokens

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,9 +52,62 @@ local setup_lsp_handlers = function()
     })
 end
 
+local function enable_full_semantic_tokens(client)
+  -- NOTE: Super hacky... Don't know if I like that we set a random variable on the client
+  -- Seems to work though
+  if client.is_hacked then
+    return
+  end
+  client.is_hacked = true
+
+  -- let the runtime know the server can do semanticTokens/full now
+  client.server_capabilities = vim.tbl_deep_extend('force', client.server_capabilities, {
+    semanticTokensProvider = {
+      full = true,
+    },
+  })
+
+  -- monkey patch the request proxy
+  local request_inner = client.request
+  client.request = function(method, params, handler, req_bufnr)
+    if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
+      return request_inner(method, params, handler)
+    end
+
+    local function find_buf_by_uri(search_uri)
+      local bufs = vim.api.nvim_list_bufs()
+      for _, buf in ipairs(bufs) do
+        local name = vim.api.nvim_buf_get_name(buf)
+        local uri = 'file://' .. name
+        if uri == search_uri then
+          return buf
+        end
+      end
+    end
+
+    local target_bufnr = find_buf_by_uri(params.textDocument.uri)
+    local line_count = vim.api.nvim_buf_line_count(target_bufnr)
+    local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
+
+    return request_inner('textDocument/semanticTokens/range', {
+      textDocument = params.textDocument,
+      range = {
+        ['start'] = {
+          line = 0,
+          character = 0,
+        },
+        ['end'] = {
+          line = line_count - 1,
+          character = string.len(last_line) - 1,
+        },
+      },
+    }, handler, req_bufnr)
+  end
+end
+
 local on_attach = function(client, bufnr)
   local utils = require 'utils'
-  client.server_capabilities.semanticTokensProvider = vim.NIL
+  enable_full_semantic_tokens(client)
 
   if client.server_capabilities.inlayHintProvider then
     vim.lsp.inlay_hint.enable(true)
@@ -138,7 +191,6 @@ return {
             -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
-        tsserver = {},
         prismals = {},
         jdtls = {},
       }
@@ -153,7 +205,6 @@ return {
       )
       capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
       -- capabilities.textDocument.completion.completionItem.snippetSupport = false
-      capabilities.textDocument.semanticTokens = nil
 
       -- Ensure the servers above are installed
       local mason_lspconfig = require 'mason-lspconfig'
@@ -198,7 +249,7 @@ return {
           on_attach = on_attach,
           filetypes = { 'cs' },
           settings = {
-            ["csharp|inlay_hints"] = {
+            ['csharp|inlay_hints'] = {
               csharp_enable_inlay_hints_for_implicit_object_creation = true,
               csharp_enable_inlay_hints_for_implicit_variable_types = true,
               csharp_enable_inlay_hints_for_lambda_parameter_types = true,
@@ -212,12 +263,11 @@ return {
               dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
               dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
             },
-            ["csharp|code_lens"] = {
+            ['csharp|code_lens'] = {
               dotnet_enable_references_code_lens = true,
             },
-          }
-
-        }
+          },
+        },
       }
     end,
   },
