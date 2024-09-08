75c8c88d6089d98d38f7b5b30c3b585263a97ee4
İlyas Akın <ashnwor@gmail.com>
2024-09-08 21:16:53 +0300

    Add beacon.vim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -33,8 +33,9 @@ return {
   },
 
   {
-    "benlubas/molten-nvim",
-        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-    build = ":UpdateRemotePlugins",
-  }
+    'benlubas/molten-nvim',
+    version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
+    build = ':UpdateRemotePlugins',
+  },
+  { 'danilamihailov/beacon.nvim' },
 }
