6f3667949bf9373f56e555027873d1b94c6a55b7
İlyas Akın <ilyas.akin@kuika.com>
2024-11-29 09:44:15 +0300

    Use catpucchin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,11 +13,20 @@ return {
   --   end,
   -- },
 
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   config = function()
+  --     vim.cmd 'colorscheme rose-pine'
+  --   end,
+  -- },
+
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-      vim.cmd 'colorscheme rose-pine'
-    end,
-  },
+      vim.cmd "colorscheme catppuccin-mocha"
+    end
+  }
 }
