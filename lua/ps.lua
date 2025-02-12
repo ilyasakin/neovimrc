9fa74dfb61d16a19f699992261ecaf0137ad4a26
İlyas Akın <ilyas.akin@kuika.com>
2025-02-12 10:37:26 +0300

    feat: improve process selection with Windows support and better output

--- lua/ps.lua
+++ lua/ps.lua
@@ -7,29 +7,32 @@ function M.pick()
   local actions = require 'telescope.actions'
   local action_state = require 'telescope.actions.state'
 
-  local input = { 'ps', '-eo', 'pid,args' }
+  local input
+  if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+    input = { 'powershell', '-NoProfile', '-Command', 'Get-Process | Select-Object Id, ProcessName, CommandLine | Format-Table -AutoSize' }
+  else
+    input = { 'ps', '-eo', 'pid,comm,args', '--no-headers' }
+  end
 
   local opts = {
+    prompt_title = 'Select Process',
     finder = finders.new_oneshot_job(input, {}),
     sorter = sorters.get_generic_fuzzy_sorter(),
-  }
-
-  local value = nil
-  local co = coroutine.running()
-  local picker = pickers.new(opts, {
     attach_mappings = function(prompt_bufnr, _)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        coroutine.resume(co, selection)
+        coroutine.resume(coroutine.running(), selection)
       end)
       return true
     end,
-  })
+  }
+
+  local picker = pickers.new(opts)
   picker:find()
-  value = coroutine.yield()
+  local value = coroutine.yield()
   value = vim.trim(value[1])
-  local parts = vim.split(value, ' ')
+  local parts = vim.split(value, '%s+')
   return tonumber(parts[1])
 end
 
