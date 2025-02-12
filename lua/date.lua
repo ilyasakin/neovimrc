3cca7024ba0010d69b5e4ea2a45dc5609578f52e
İlyas Akın <ilyas.akin@kuika.com>
2025-02-12 11:44:17 +0300

    feat: Add iso date command

--- lua/date.lua
+++ lua/date.lua
@@ -1,13 +1,8 @@
 local M = {}
 
-function M.pick()
+function M.iso()
   local date = os.date('%Y-%m-%dT%H:%M:%S')
-  return date
+  vim.api.nvim_put({ date }, '', true, true)
 end
-
-vim.api.nvim_create_user_command('DateIsoNew', function()
-  local result = M.pick()
-  vim.api.nvim_put({ result }, '', true, true)
-end, {})
 
 return M 
